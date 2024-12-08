86d4244bc299c7c3c5fcffd6e958519ce1796a0e
F1reC <1489472989@qq.com>
2024-12-08 18:39:52 +0800

    Initial Commit

---
+++ Fate_islandTests/Fate_islandTests.swift
@@ -0,0 +1,17 @@
+//
+//  Fate_islandTests.swift
+//  Fate_islandTests
+//
+//  Created by F1reC on 2024/12/8.
+//
+
+import Testing
+@testable import Fate_island
+
+struct Fate_islandTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
